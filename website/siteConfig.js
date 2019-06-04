fc52e6ad0bb27c8a96643c0b4dd4cb3cf93a0611
Carlo Di Maio <carlo.dimaio@gmail.com>
2019-06-04 18:38:53 +0200

    initial import

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -24,7 +24,7 @@ const siteConfig = {
   title: 'ReStation API', // Title for your website.
   tagline: 'A website for testing',
   url: 'https://restation.github.io', // Your website URL
-  baseUrl: '/restation-resources', // Base URL for your project */
+  baseUrl: '/restation-resources/', // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
